a5e5a698f6e7c3ca58eeff4e505db76d8a00e34a
btresca <132278107+btresca@users.noreply.github.com>
2024-08-26 09:30:19 -0400

    Fix transform

--- read_grade.py
+++ read_grade.py
@@ -1,20 +1,73 @@
 ## Script to scan all PDFs in directory for grades
 ## Assumes that pdf_ocn_rename already run so file name is formeted: assessment_OCN.pdf
 ## v2 - uses pandas and sorts grades by OCN
-import os
+## v3 - fixed four_point_transform error, reads five grade levels A-F
 
+import os
 import cv2
 import fitz
 import imutils
 import numpy as np
 import pandas as pd
-from imutils.perspective import four_point_transform
 from PIL import Image
 
 # Uncomment to run for single pdf file when testing
 # pdf_file = "test_scan.pdf"
 
 mat = fitz.Matrix(2, 2)
+
+def order_points(pts):
+	# initialzie a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype = "float32")
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+	# return the ordered coordinates
+	return rect
+
+def four_point_transform(image, pts):
+	# obtain a consistent order of the points and unpack them
+	# individually
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+	# compute the width of the new image, which will be the
+	# maximum distance between bottom-right and bottom-left
+	# x-coordiates or the top-right and top-left x-coordinates
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+	# compute the height of the new image, which will be the
+	# maximum distance between the top-right and bottom-right
+	# y-coordinates or the top-left and bottom-left y-coordinates
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+	# now that we have the dimensions of the new image, construct
+	# the set of destination points to obtain a "birds eye view",
+	# (i.e. top-down view) of the image, again specifying points
+	# in the top-left, top-right, bottom-right, and bottom-left
+	# order
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+	# compute the perspective transform matrix and then apply it
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	# return the warped image
+	return warped
 
 # Apply to all pdfs in current directory
 pdf_files = [filename for filename in os.listdir(
@@ -86,10 +139,8 @@ for pdf_file in pdf_files:
         print("No grades in", pdf_file)
         continue
 
-## Problem with four_point_transform is causing some files to crop outside the box, actual problem is contour list (docCnt)
-    #warped = four_point_transform(grey, docCnt.reshape(4, 2))
-    warped = grey[docCnt.reshape(4,2)]
-    ## work here to fix the transform output coordinates
+## Fixed four_point_transform crop error by def func above
+    warped = four_point_transform(grey, docCnt.reshape(4, 2))
     crop = 3
     y1 = paper.shape[0]
     x1 = paper.shape[1]
@@ -98,12 +149,12 @@ for pdf_file in pdf_files:
     x1 = warped.shape[1]
     warped = warped[crop:y1-crop, crop:x1-crop]
 
-    cv2.imshow(pdf_file, paper)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    cv2.imshow(pdf_file, warped)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.imshow(pdf_file, paper)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    #cv2.imshow(pdf_file, warped)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
     #break
 
     # apply Otsu's thresholding method to binarize the warped
